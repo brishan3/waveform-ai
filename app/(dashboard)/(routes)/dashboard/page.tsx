9b711bda84f0cfd3249cf7caab845882278bcc4e
Brishan King <brishan.king@gmail.com>
2023-08-01 16:57:16 -0700

    Initialize project; install Tailwind, shadcn, TypeScript and Clerk; scaffold routes for Dashboard and Landing page

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+const DashboardPage = () => {
+  return (
+    <div>Dashboard Page (Protected)</div>
+  )
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+
+
+const LandingPage = () => {
+  return (
+    <div>
+      Landing Page (Unprotected)
+    </div>
+  )
+}
+
+export default LandingPage;
