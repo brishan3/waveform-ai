f33c107fb0f4eadaa2c5ba0c2eeca6586df068eb
Brishan King <brishan.king@gmail.com>
2023-08-02 11:37:09 -0700

    Add Shadcn Form, Card, Sheet and Input components; start building out Conversation page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,91 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+import {
+  LayoutDashboard,
+  MessageSquare,
+  ImageIcon,
+  VideoIcon,
+  Music,
+  Code,
+  Settings,
+  ArrowRight
+} from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10"
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10"
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-orange-700",
+    bgColor: "bg-orange-700/10"
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    href: "/music",
+    color: "text-emerald-500",
+    bgColor: "bg-emerald-500/10"
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    href: "/code",
+    color: "text-green-700",
+    bgColor: "bg-green-700/10"
+  },
+]
 
 const DashboardPage = () => {
+
+  const router = useRouter();
+
   return (
     <div>
-      <p>Dashboard Page (Protected)</p>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl fon-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Interface with the latest generative AI models for any task.
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map(({ label, icon: Icon, href, color, bgColor }) => (
+          <Card
+            key={href}
+            onClick={() => router.push(href)}
+            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+          >
+            <div className="flex items-center gap-4">
+              <div className={cn("p-2 w-fit rounded-md", bgColor)}>
+                <Icon className={cn("w-8 h-8", color)}/>
+              </div>
+              <div className="font-semibold">
+                {label}
+              </div>
+              <ArrowRight className="w-5 h-5"/>
+            </div>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,63 +1,110 @@
 "use client";
 
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
 
-import { cn } from '@/lib/utils';
-import { Montserrat } from 'next/font/google';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { LayoutDashboard, MessageSquare } from "lucide-react";
+import { cn } from "@/lib/utils";
+import {
+  LayoutDashboard,
+  MessageSquare,
+  ImageIcon,
+  VideoIcon,
+  Music,
+  Code,
+  Settings,
+} from "lucide-react";
 
 const montserrat = Montserrat({
   weight: "600",
-  subsets: ["latin"]
-})
+  subsets: ["latin"],
+});
 
 const routes = [
   {
     label: "Dashboard",
     icon: LayoutDashboard,
     href: "/dashboard",
-    color: "text-sky-500"
+    color: "text-sky-500",
   },
   {
     label: "Conversation",
     icon: MessageSquare,
     href: "/conversation",
-    color: "text-violet-500"
+    color: "text-violet-500",
   },
-]
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image",
+    color: "text-pink-700",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-orange-700",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    href: "/music",
+    color: "text-emerald-500",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    href: "/code",
+    color: "text-green-700",
+  },
+  {
+    label: "Settings",
+    icon: Settings,
+    href: "/settings",
+    color: "text-white",
+  },
+];
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
-      <div className='px-3 py-2 flex-1'>
+      <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-3 mb-14">
           <div className="relative w-14 h-12 mr-4">
             <Image
               fill
               alt="Logo"
               src="/logo.webp"
-              className='object-contain'
+              className="object-contain"
             />
           </div>
-          <h1 className={cn("text-2xl font-bold", montserrat.className)}>Wave<span className='text-blue-200'>form</span></h1>
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            Wave<span className="text-blue-200">form</span>
+          </h1>
         </Link>
-        <div className='space-y-1'>
-          {routes.map(({label, icon: Icon, href, color}) => (
-            <Link href={href} key={href} className='text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition'>
-              <div className='flex items-center flex-1'>
-                <Icon className={cn("h-5 w-5 mr-3", color)}/>
+        <div className="space-y-1">
+          {routes.map(({ label, icon: Icon, href, color }) => (
+            <Link
+              href={href}
+              key={href}
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === href ? "text-white bg-white/10" : "text-zinc-400"
+              )}
+            >
+              <div className="flex items-center flex-1">
+                <Icon className={cn("h-5 w-5 mr-3", color)} />
                 {label}
               </div>
             </Link>
-          ))
-
-          }
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
