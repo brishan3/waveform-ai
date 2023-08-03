8d2afe0e94d9d9110e741114a527da9c1a3f7fc3
Brishan King <brishan.king@gmail.com>
2023-08-03 11:41:35 -0700

    Add gradient to Landing page layout

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -4,7 +4,7 @@ const LandingLayout = ({
   children: React.ReactNode;
 }) => {
   return (
-    <main className="h-full bg-[#111827] overflow-auto">
+    <main className="h-full bg-gradient-to-br from-[#111827] to-gray-800 overflow-auto">
       <div className="mx-auto max-w-screen-xl h-full w-full">
         {children}
       </div>
