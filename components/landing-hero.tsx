4a0ea16a24240a866121debf3cef9e0b5db76d51
Brishan King <brishan.king@gmail.com>
2023-08-03 10:11:30 -0700

    Update landing-hero styles

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -9,7 +9,7 @@ export const LandingHero = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <div className="text-white font-bold py-36 text-center space-y-5">
+    <div className="text-white font-bold text-center py-16 space-y-5 min-h-[80vh] flex flex-col justify-center items-center">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold px-4 sm:px-6">
         <h1>Powered by the best AI models for</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-br from-[#dd876c] to-blue-700 pl-3">
